e1b48e889f0697632029dbd8a32a9bde2b070b76
Isaac22 <isaacgomezvallejos@gmail.com>
2025-03-24 13:22:27 +0100

    Create Operadores.java

---
+++ Operadores.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Operadores {
+    public int suma(int n, int n2){return n+n2;}
+
+    public int resta(int n, int n2){
+        return n-n2;
+    }
+
+    public int multi(int n, int n2){
+        return n*n2;
+    }
+
+    public int div(int n, int n2){
+        return n/n2;
+    }
+}
